9d19f5d1750d1c374a4864f645965ba0ccf8c45f
Ganesh Tarone <62292892+GTron-1729@users.noreply.github.com>
2022-02-22 15:15:26 +0530

    first_bad_version.cpp

---
+++ first_bad_version.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int ans = 0,low = 1, high = n;
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(isBadVersion(mid) == false) low = mid + 1;
+            else{
+                high = mid -1;
+                ans = mid;
+            }
+        }
+        return ans;
+    }
+};
